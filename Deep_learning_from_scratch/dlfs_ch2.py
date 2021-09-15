f65e90b03e415d7d971f502dd6f11532338d351c
sunghyouk <sakebite@snu.ac.kr>
2021-09-15 16:22:21 +0900

    Create dlfs_ch2.py

---
+++ Deep_learning_from_scratch/dlfs_ch2.py
@@ -0,0 +1,52 @@
+# Perceptron AND gate
+# 2.3.1 Formula 2.1
+def AND(x1, x2):
+    w1, w2, theta = 0.5, 0.5, 0.7
+    tmp = x1*w1 + x2*w2
+    if tmp <= theta:
+        return 0
+    if tmp > theta:
+        return 1
+
+# 2.3.2 Weight, Bias
+# Formula 2.2
+import numpy as np
+x = np.array([0, 1]) #입력
+w = np.array([0.5, 0.5]) #가중치
+b = -0.7 # - theta = b (편향)
+w*x
+np.sum(w*x)
+np.sum(w*x)+b
+
+# 2.3.3 가중치와 편향 구현하기
+def AND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.7
+    tmp = np.sum(w*x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+# NAND
+def NAND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([-0.5, -0.5])
+    b = 0.7
+    tmp = np.sum(w*x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+# OR
+def OR(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.2
+    tmp = np.sum(w*x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
