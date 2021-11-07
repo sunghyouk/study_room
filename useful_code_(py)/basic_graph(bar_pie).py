59ccb6a1d7b3c7fee2dee8c2a6b4eb77b2f3ddd4
sunghyouk <sakebite@snu.ac.kr>
2021-11-07 17:47:03 +0900

    correct grammar

--- useful_code_(py)/basic_graph(bar_pie).py
+++ useful_code_(py)/basic_graph(bar_pie).py
@@ -2,7 +2,7 @@ import pandas as pd
 fifa2019 = pd.read_csv("fifa2019.csv")
 
 # bar graph
-fifa2019['Preferred Foot'].value_count().plot(kind = 'bar')
+fifa2019['Preferred Foot'].value_count().plot(kind='bar')
 
 # pie graph
-fifa2019['Preferred Foot'].value_count().plot(kind = 'pie', autopct = '%1.f% %')
+fifa2019['Preferred Foot'].value_count().plot(kind='pie', autopct='%1.f% %')
