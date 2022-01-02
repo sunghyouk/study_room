36ac22855847fa0085cf97ecf387ec17a1cfbc1b
sunghyouk <sakebite@snu.ac.kr>
2022-01-02 09:47:34 +0900

    update codes

--- Deep_learning_from_scratch2/ch01/train.py
+++ Deep_learning_from_scratch2/ch01/train.py
@@ -3,12 +3,14 @@
 # 1.4.3 학습용 코드 (train_custom_loop.py)와 같은 결과
 import sys
 
-sys.path.append('..')  # 부모 디렉터리의 파일을 가져올 수 있도록 설정
 from common.optimizer import SGD
 from common.trainer import Trainer
 from dataset import spiral
 
 from two_layer_net import TwoLayerNet
+
+sys.path.append('..')  # 부모 디렉터리의 파일을 가져올 수 있도록 설정
+
 
 # 하이퍼파라미터 설정
 max_epoch = 300
