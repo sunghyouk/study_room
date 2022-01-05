11b9154f22b56d175c15cebbdc9c4788d4115edf
sunghyouk <sakebite@snu.ac.kr>
2022-01-05 13:03:32 +0900

    update codes

--- python_ML/ex_chapter_2.py
+++ python_ML/ex_chapter_2.py
@@ -9,6 +9,7 @@ from matplotlib import markers
 
 import python_ML.plot_decision_regions as pltreg
 from python_ML.adaline import AdalineGD
+from python_ML.adalinesgd import AdalineSGD
 from python_ML.perceptron import Perceptron
 
 s = os.path.join('https://archive.ics.uci.edu', 'ml',
@@ -97,5 +98,22 @@ plt.show()
 plt.plot(range(1, len(ada.cost_) + 1), ada.cost_, marker='o')
 plt.xlabel('Epochs')
 plt.ylabel('Sum-squarred-error')
+plt.tight_layout()
+plt.show()
+
+# AdalineSGD 모델 훈련, 그래프 그리기
+ada = AdalineSGD(n_iter=15, eta=0.01, random_state=1)
+ada.fit(X_std, y)
+
+pltreg.plot_decision_regions(X_std, y, classifier=ada)
+plt.title('Adaline - Stochastic Gradient Descent')
+plt.xlabel('Sepal length [Standardized]')
+plt.ylabel('Petal length [Standardized]')
+plt.legend(loc='upper left')
+plt.show()
+
+plt.plot(range(1, len(ada.cost_) + 1), ada.cost_, marker='o')
+plt.xlabel('Epochs')
+plt.ylabel('Average Cost')
 plt.tight_layout()
 plt.show()
