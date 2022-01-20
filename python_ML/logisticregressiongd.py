77955c582fef8445f32460518865d126738953f0
sunghyouk <sakebite@snu.ac.kr>
2022-01-20 11:05:58 +0900

    update codes

--- python_ML/logisticregressiongd.py
+++ python_ML/logisticregressiongd.py
@@ -5,17 +5,22 @@ class LogisticRegressionGD(object):
     """경사 하강법을 사용한 로지스틱 회귀 분류기
 
     매개변수
-    --------
-    eta : float 학습률 (0.0과 1.0 사이)
-    n_iter : int 훈련 데이터셋 반복 횟수
-    random_state : int 가중치 무작위 초기화를 위한 난수 생성기 시드
+    ------------
+    eta : float
+      학습률 (0.0과 1.0 사이)
+    n_iter : int
+      훈련 데이터셋 반복 횟수
+    random_state : int
+      가중치 무작위 초기화를 위한 난수 생성기 시드
 
     속성
-    --------
-    w_ : 1d_array 학습된 가중치
-    cost_ : list 에포크마다 누적된 로지스틱 비용 함수 값
+    -----------
+    w_ : 1d-array
+      학습된 가중치
+    cost_ : list
+      에포크마다 누적된 로지스틱 비용 함수 값
 
-   """
+    """
     def __init__(self, eta=0.05, n_iter=100, random_state=1):
         self.eta = eta
         self.n_iter = n_iter
@@ -25,20 +30,19 @@ class LogisticRegressionGD(object):
         """훈련 데이터 학습
 
         매개변수
-        ---------
-            X : {array-like}, shape = [n_samples, n_features]
-              n_samples개의 샘플과 n_features개의 특성으로 이루어진 훈련 데이터
-            y : array-like, shape = [n_samples]
-              타깃 값
+        ----------
+        X : {array-like}, shape = [n_samples, n_features]
+          n_samples 개의 샘플과 n_features 개의 특성으로 이루어진 훈련 데이터
+        y : array-like, shape = [n_samples]
+          타깃값
 
         반환값
-        ---------
+        -------
         self : object
 
         """
         rgen = np.random.RandomState(self.random_state)
-        self.w_ = rgen.normal(loc=0.0, scale=0.01,
-                              size=1 + X.shape[1])
+        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=1 + X.shape[1])
         self.cost_ = []
 
         for i in range(self.n_iter):
@@ -48,8 +52,8 @@ class LogisticRegressionGD(object):
             self.w_[1:] += self.eta * X.T.dot(errors)
             self.w_[0] += self.eta * errors.sum()
 
-            # 제곱 오차항 대신 로지스틱 비용을 계산
-            cost = (-y.dot(np.log(output))) - ((1-y).dot(np.log(1 - output)))
+            # 오차 제곱합 대신 로지스틱 비용을 계산합니다.
+            cost = -y.dot(np.log(output)) - ((1 - y).dot(np.log(1 - output)))
             self.cost_.append(cost)
         return self
 
@@ -59,8 +63,11 @@ class LogisticRegressionGD(object):
 
     def activation(self, z):
         """로지스틱 시그모이드 활성화 계산"""
-        return 1. / (1. + np.exp(np.clip(z, -250, 250)))
+        # 대신 from scipy.special import expit; expit(z) 을 사용할 수 있습니다.
+        return 1. / (1. + np.exp(-np.clip(z, -250, 250)))
 
     def predict(self, X):
-        """단위 계단 함수를 사용하여 클래스 레이블을 반환"""
+        """단위 계단 함수를 사용하여 클래스 레이블을 반환합니다"""
         return np.where(self.net_input(X) >= 0.0, 1, 0)
+        # 다음과 동일합니다.
+        # return np.where(self.activation(self.net_input(X)) >= 0.5, 1, 0)
--- python_ML/plot_decision_regions.py
+++ python_ML/plot_decision_regions.py
@@ -3,15 +3,14 @@ import numpy as np
 from matplotlib.colors import ListedColormap
 
 
-def plot_decision_regions(X, y, classifier, test_idx=None,
-                          resolution=0.02):
+def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
 
-    # 마커와 칼라맵을 설정
+    # 마커와 컬러맵을 설정합니다.
     markers = ('s', 'x', 'o', '^', 'v')
     colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
     cmap = ListedColormap(colors[:len(np.unique(y))])
 
-    # 결정 경계를 그리기
+    # 결정 경계를 그립니다.
     x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
     x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
     xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
@@ -22,7 +21,6 @@ def plot_decision_regions(X, y, classifier, test_idx=None,
     plt.xlim(xx1.min(), xx1.max())
     plt.ylim(xx2.min(), xx2.max())
 
-    # 샘플의 산점도 그리기
     for idx, cl in enumerate(np.unique(y)):
         plt.scatter(x=X[y == cl, 0],
                     y=X[y == cl, 1],
@@ -30,13 +28,19 @@ def plot_decision_regions(X, y, classifier, test_idx=None,
                     c=colors[idx],
                     marker=markers[idx],
                     label=cl,
-                    edgecolor='black')
+                    edgecolor=None if idx == 1 else 'black')
 
-    # 테스트 샘플을 부각하여 그리기
+    # 테스트 샘플을 부각하여 그립니다.
     if test_idx:
+        # 모든 샘플을 그립니다.
         X_test, y_test = X[test_idx, :], y[test_idx]
 
-        plt.scatter(X_test[:, 0], X_test[:, 1],
-                    facecolors='none', edgecolors='black', alpha=1.0,
-                    linewidth=1, marker='o',
-                    s=100, label='test set')
+        plt.scatter(X_test[:, 0],
+                    X_test[:, 1],
+                    facecolor='none',
+                    edgecolor='black',
+                    alpha=1.0,
+                    linewidth=1,
+                    marker='o',
+                    s=100,
+                    label='test set')
