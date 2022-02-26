2511f6093a55592bc3df1380231ba796a912333f
sunghyouk <sakebite@snu.ac.kr>
2022-02-26 23:17:02 +0900

    update codes (linear algebra)

--- linalg_python/util.py
+++ linalg_python/util.py
@@ -33,3 +33,47 @@ def deepcopy(A):
         for i in range(0, n):
             res.append(A[i])
         return res
+
+
+def u_tri(A):
+    """
+    상 삼각 행렬 변환
+    입력값: 상 삼각 행렬로 변환하고자 하는 행렬 A
+    출력값: 행렬 A를 상 삼각 행렬로 변환시킨 행렬 utri
+    """
+
+    n = len(A)
+    p = len(A[0])
+    utri = []
+
+    for i in range(0, n):
+        row = []
+        for j in range(0, p):
+            if i > j:
+                row.append(0)
+            else:
+                row.append(A[i][j])
+        utri.append(row)
+    return utri
+
+
+def l_tri(A):
+    """
+    하 삼각 행렬 변환
+    입력값: 하 삼각 행렬로 변환하고자 하는 행렬 A
+    출력값: 행렬 A를 하 삼각 행렬로 변환시킨 행렬 ltri
+    """
+
+    n = len(A)
+    p = len(A[0])
+    ltri = []
+
+    for i in range(0, n):
+        row = []
+        for j in range(0, p):
+            if i < j:
+                row.append(0)
+            else:
+                row.append(A[i][j])
+        ltri.append(row)
+    return ltri
