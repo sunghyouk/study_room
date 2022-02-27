ba880c6f5ea01dfa4ce205fd58700e53780962ae
sunghyouk <sakebite@snu.ac.kr>
2022-02-27 23:24:01 +0900

    update code (correct grammar)

--- modern_python/ch6.py
+++ modern_python/ch6.py
@@ -1,12 +1,12 @@
-minutes_to_convert = 789 # 입력부분
+minutes_to_convert = 789  # 입력부분
 
-hours_decimal = minutes_to_convert / 60 # 시간부분 (정수)
+hours_decimal = minutes_to_convert / 60  # 시간부분 (정수)
 hours_part = int(hours_decimal)
 
-minutes_decimal = hours_decimal - hours_part # 분 부분 (소수점 * 60의 결과값 반올림)
+minutes_decimal = hours_decimal - hours_part  # 분 부분 (소수점 * 60의 결과값 반올림)
 minutes_part = round(minutes_decimal * 60)
 
-print(hours_part) # 출력부분
+print(hours_part)  # 출력부분
 print("시간")
 print(minutes_part)
 print("분")
