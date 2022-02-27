3352bd1603b763ca13d64768a70667e0757e65d4
sunghyouk <sakebite@snu.ac.kr>
2022-02-27 23:23:25 +0900

    update codes (blockchain)

--- blockchain/1_1_bitcoin_nodes.py
+++ blockchain/1_1_bitcoin_nodes.py
@@ -1,7 +1,8 @@
 # 파이썬 실습 파일: 1-1.BitcoinNodes.py
-import requests
 import time
+
 import matplotlib.pyplot as plt
+import requests
 
 # 100 page까지만 조회한다. 이 사이트는 최근 60일까지 데이터를 제공함.
 nPage = 100
@@ -16,17 +17,17 @@ else:
         resp = requests.get(url=url)
         data = resp.json()
         print("page %d loaded." % page)
-        
+
         for i in range(len(data['results'])):
             ts = time.gmtime(data['results'][i]['timestamp'])
             t.append(time.strftime("%Y-%m-%d %H:%M:%S", ts))
             n.append(data['results'][i]['total_nodes'])
-    
+
     t = t[::-1]
     n = n[::-1]
-    
+
     # 최근 노드수의 변화를 확인한다
-    plt.figure(figsize=(8,6))
+    plt.figure(figsize=(8, 6))
     plt.plot(n, color='red', linewidth=0.7)
     plt.title('Bitcoin Nodes\n' + t[0] + '~' + t[-1])
     plt.grid(color='green', alpha=0.2)
