b64a5b7b9dbb9f84fd9d0dafb5898f30ccbec16f
sunghyouk <sakebite@snu.ac.kr>
2022-03-04 23:39:49 +0900

    update codes

--- linalg_python/util.py
+++ linalg_python/util.py
@@ -77,3 +77,111 @@ def l_tri(A):
                 row.append(A[i][j])
         ltri.append(row)
     return ltri
+
+
+def det_rec(A):
+
+    """
+    행렬 A의 행렬식 구하기 (재귀 방식을 이용)
+    입력값: 행렬식을 구하고자 하는 행렬 A
+    출력값: 행렬 A의 행렬식 res
+    """
+
+    n = len(A)
+    res = 0
+
+    # 2*2 행렬의 행렬식 구하기
+    if n == 2:
+        res = A[0][0] * A[1][1] - A[1][0] * A[0][1]
+        return res
+
+    # n*n 행렬의 행렬식 구하기
+    for i in range(0, n):
+        X = deepcopy(A)
+        X = X[1:]
+        nx = len(X)
+
+        for j in range(0, nx):
+            X[j] = X[j][0:i] + X[j][i+1:]
+
+        sign = (-1) ** (i % 2)
+        sub_det = det_rec(X)
+        res += sign * A[0][i] * sub_det
+
+    return res
+
+
+def transpose(A):
+    """
+    행렬의 전치 행렬
+    입력값: 전치 행렬을 구하고자 하는 행렬 A
+    출력값: 행렬 A의 전치 행렬 At
+    """
+
+    n = len(A)
+    p = len(A[0])
+
+    At = []
+    for i in range(0, p):
+        row = []
+        for j in range(0, n):
+            val = A[j][i]
+            row.append(val)
+        At.append(row)
+    return At
+
+
+def scalar_mul(b, A):
+    """
+    행렬의 스칼라 곱
+    입력값: 스칼라 곱을 수행할 스칼라 b, 행렬 A
+    출력값: 스칼라 b와 행렬 A의 스칼라 곱 결과인 행렬 C
+    """
+
+    n = len(A)
+    p = len(A[0])
+
+    res = []
+    for i in range(0, n):
+        row = []
+        for j in range(0, p):
+            val = b * A[i][j]
+            row.append(val)
+        res.append(row)
+    return res
+
+
+def inv(A):
+    """
+    행렬 A의 역행렬 구하기
+    입력값: 행렬 A
+    출력값: 행렬 A의 역행렬 res
+    """
+
+    n = len(A)
+    X = deepcopy(A)
+
+    C = []
+    for i in range(0, n):
+        row_C = []
+        idx_r = list(range(0, n))
+        idx_r.remove(i)
+        for j in range(0, n):
+            idx_c = list(range(0, n))
+            idx_c.remove(j)
+            M = []
+            for k in idx_r:
+                row_M = []
+                for l in idx_c:
+                    val = X[k][l]
+                    row_M.append(val)
+                M.append(row_M)
+            Mij = det_rec(M)
+            Cij = ((-1)**(i+j))*Mij
+            row_C.append(Cij)
+        C.append(row_C)
+
+    adj = transpose(C)
+    res = scalar_mul(1/det_rec(X), adj)
+
+    return res
