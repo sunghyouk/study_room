fc6187225aab9a3068ee42d3181494a3102e174d
sunghyouk <sakebite@snu.ac.kr>
2022-03-06 10:58:00 +0900

    update example codes

---
+++ py_automation/vampire2.py
@@ -0,0 +1,13 @@
+name = 'Carol'
+age = 3000
+
+if name == 'Alice':
+    print('Hi, Alice.')
+elif age < 12:
+    print('You are not Alice, kiddo.')
+elif age > 100:
+    print('You are not Alice, grannie.')
+elif age > 2000:
+    print('Unlike you, Alice is not an undead, immortal vampire.')
+
+# age > 100의 코드 블록이 출력
