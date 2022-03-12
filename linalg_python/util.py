a151c6172d49b300bd90e0b617b77d0ba6fa4111
sunghyouk <sakebite@snu.ac.kr>
2022-03-12 23:21:29 +0900

    update codes

--- linalg_python/util.py
+++ linalg_python/util.py
@@ -185,3 +185,105 @@ def inv(A):
     res = scalar_mul(1/det_rec(X), adj)
 
     return res
+
+
+def inner_product(a, b):
+    """
+    벡터의 내적
+    입력값: 내적할 벡터 리스트 a, b
+    출력값: 벡터 a, b의 내적 결과 res
+    """
+    n = len(a)
+    res = 0
+    for i in range(0, n):
+        res += a[i] * b[i]
+    return res
+
+
+def norm(a):
+    """
+    벡터의 norm
+    입력값: norm을 구하고자 하는 벡터 a
+    출력값: 벡터 a의 norm 결과 res
+    """
+
+    n = len(a)
+    res = 0
+    for i in range(0, n):
+        res += a[i]**2
+    res = res**0.5
+    return res
+
+
+def normalize(a):
+    """
+    벡터 a의 normalization
+    벡터 a의 norm을 1로 만들어 줌
+    입력값: normalize할 벡터 리스트 a
+    출력값: 벡터 a를 normalization한 결과 벡터 리스트 v
+    """
+    n = len(a)
+    v = []
+
+    for i in range(0, n):
+        tmp = a[i]/norm(a)
+        v.append(tmp)
+    return v
+
+
+def qr_gram(A):
+    """
+    그램 슈미트 과정을 이용한 QR분해
+    입력값: 행렬 A
+    출력값: 행렬 A를 그램 슈미트 과정을 이용해 QR분해한 결과행렬 Q, R
+    """
+
+    n = len(A)
+    At = transpose(A)
+
+    U = []
+    norm_list = []
+
+    V = []
+    Q = []
+    R = []
+
+    for i in range(0, n):
+        if i == 0:
+            u = At[i]
+            norm_u = norm(u)
+            U.append(u)
+            norm_list.append(norm_u)
+        else:
+            a = At[i]
+            dp_list = []
+            for j in range(0, i):
+                dp = inner_product(a, U[j])
+                dp_list.append(dp)
+
+            u = []
+            for j in range(0, n):
+                val = a[j]
+                for k in range(0, i):
+                    val -= (dp_list[k]/norm_list[k]**2) * U[k][j]
+                u.append(val)
+            norm_u = norm(u)
+            U.append(u)
+            norm_list.append(norm_u)
+
+        v = normalize(u)
+        V.append(v)
+
+    Q = transpose(V)
+
+    for i in range(0, n):
+        r = []
+        for j in range(0, n):
+            if i > j:
+                r.append(0)
+            else:
+                r_ele = inner_product(At[j], V[i])
+                r.append(r_ele)
+        R.append(r)
+
+    return Q, R
