5eefa03556d1d4f3a0817b441dc35a3018bd31c0
sunghyouk <sakebite@snu.ac.kr>
2022-04-15 07:17:00 +0900

    update movie classification app

--- python_ML/movieclassifier/app.py
+++ python_ML/movieclassifier/app.py
@@ -1,4 +1,3 @@
-import imp
 import os
 import pickle
 import sqlite3
@@ -40,4 +39,47 @@ def sqlite_entry(path, document, y):
     conn.commit()
     conn.close()
 
-# TODO: making class for flask
+
+# Flask
+class ReviewForm(Form):
+    moviereview = TextAreaField('',
+                                [validators.DataRequired(),
+                                 validators.length(min=15)])
+
+
+@app.route('/')
+def index():
+    form = ReviewForm(request.form)
+    return render_template('reviewform.html', form=form)
+
+
+@app.route('/results', methods=['POST'])
+def results():
+    form = ReviewForm(request.form)
+    if request.method == 'POST' and form.validate():
+        review = request.form['moviereview']
+        y, proba = classify(review)
+        return render_template('results.html',
+                               content=review,
+                               prediction=y,
+                               probability=round(proba*100, 2))
+    return render_template('reviewform.html', form=form)
+
+
+@app.route('/thanks', method=['POST'])
+def feedback():
+    feedback = request.form['feedback_button']
+    review = request.form['review']
+    prediction = request.form['prediction']
+
+    inv_label = {'negative': 0, 'positive': 1}
+    y = inv_label[prediction]
+    if feedback == 'Incorrect':
+        y = int(not(y))
+    train(review, y)
+    sqlite_entry(db, review, y)
+    return render_template('thanks.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
