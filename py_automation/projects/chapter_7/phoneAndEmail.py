05822a430516df5392e668c4b8bb603b1753e984
sunghyouk <sakebite@snu.ac.kr>
2022-04-16 23:35:35 +0900

    update regex example

--- py_automation/projects/chapter_7/phoneAndEmail.py
+++ py_automation/projects/chapter_7/phoneAndEmail.py
@@ -5,6 +5,7 @@ import re
 
 import pyperclip
 
+# 전화 번호를 위한 정규 표현식
 phoneRegex = re.compile(r'''(
     (\d{3}|\(\d{3}\))? # 지역 번호
     (\s|-|\.)? # 구분자
@@ -15,14 +16,29 @@ phoneRegex = re.compile(r'''(
 )''', re.VERBOSE)
 
 # 메일 주소를 위한 정규 표현식
-
 emailRegex = re.compile(r'''(
     [a-zA-Z0-9._%+-]+ # 사용자 이름
     @ # @ 기호
-    [a-zA-Z0-9.-] # 도메인 이름
+    [a-zA-Z0-9.-]+ # 도메인 이름
     (\.[a-zA-Z]{2,4}) # .com, .net, .org 등 최상위 도메인
 )''', re.VERBOSE)
 
-# TODO: 클립 보드의 텍스트에서 패턴이 일치하는 것 찾기
+# 클립 보드의 텍스트에서 패턴이 일치하는 것 찾기
+text = str(pyperclip.paste())
 
-# TODO: 결과를 클립보드에 복사하기
+matches = []
+for groups in phoneRegex.findall(text):
+    phoneNum = '-'.join([groups[1], groups[3], groups[5]])
+    if groups[8] != '':
+        phoneNum += ' x' + groups[8]
+    matches.append(phoneNum)
+for groups in emailRegex.finditer(text):
+    matches.append(groups[0])
+
+# 일치한 텍스트들을 클립보드에 전달
+if len(matches) > 0:
+    pyperclip.copy('\n'.join(matches))
+    print('Copied to clipboard: ')
+    print('\n'.join(matches))
+else:
+    print('No phone numbers or email address found.')
