01c707a86e74964c73415d75494b0198e500392f
sunghyouk <sakebite@snu.ac.kr>
2022-04-30 16:31:47 +0900

    modify of usage of raise, try, except

---
+++ py_automation/boxPrint.py
@@ -0,0 +1,23 @@
+# try, except
+# raise keyword의 template 같이 사용할 수 있음
+
+def boxPrint(symbol, width, height):
+    if len(symbol) != 1:
+        raise Exception('Symbol must be a single character string.')
+    if width <= 2:
+        raise Exception('Width must be greater than 2.')
+    if height <= 2:
+        raise Exception('Height must be greater than 2.')
+
+    print(symbol * width)
+
+    for i in range(height - 2):
+        print(symbol + (' ' * (width - 2)) + symbol)
+    print(symbol * width)
+
+
+for sym, w, h in (('*', 4, 4), ('0', 20, 5), ('x', 1, 3), ('ZZ', 3, 3)):
+    try:
+        boxPrint(sym, w, h)
+    except Exception as err:
+        print('An exception happended: ' + str(err))
