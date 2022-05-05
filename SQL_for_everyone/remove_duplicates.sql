e32cbd137be4ecf4b79c16e07e277686412f7c21
sunghyouk <sakebite@snu.ac.kr>
2022-05-05 23:48:44 +0900

    update explanation of code snippet

--- SQL_for_everyone/remove_duplicates.sql
+++ SQL_for_everyone/remove_duplicates.sql
@@ -1,3 +1,5 @@
+-- PostgreSQL 14에서 중복된 행을 없애는 방법
+
 -- Column에 board_pk, board_user, register_date, title, description, likes, image_name이 있을 때,
 -- title, description, register_date의 세 가지 항목으로 중복된 행을 찾아서 row_num이라는 칼럼에 중복된 횟수를 저장하고,
 -- row_num이 1보다 큰 경우를 제거하여 1만 남김
