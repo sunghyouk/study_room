457fee127315255cb8e686dc83518bb7d48770ec
sunghyouk <sakebite@snu.ac.kr>
2022-05-10 11:29:00 +0900

    update class - character/monster

--- CS_basic/python_for_CS/oop2/character.py
+++ CS_basic/python_for_CS/oop2/character.py
@@ -1,6 +1,5 @@
 # game character class
-
-from abc import *
+from abc import ABCMeta, abstractmethod
 
 
 # 추상 클래스로 character 만들기
@@ -34,7 +33,7 @@ class Player(Character):
     # 재정의된 메서드
     def attack(self, other, attack_kind):
         if attack_kind in self.skills:
-            other.get_damage(self.power, self.attack_kind)
+            other.get_damage(self.power, attack_kind)
 
     # 재정의된 메서드
     def get_damage(self, power, attack_kind):
@@ -43,5 +42,65 @@ class Player(Character):
         else:
             self.hp -= power
 
-# TODO: monster, icemonster, firemonster class
-# TODO: 연산자 오버로딩
+
+# Charracter class를 상속하여 Monster class 만들기
+class Monster(Character):
+    def __init__(self, name, hp, power):
+        super().__init__(name, hp, power)
+        self.attack_kind = 'None'
+
+    def attack(self, other, attack_kind):
+        if self.attack_kind == attack_kind:
+            other.get_damage(self.power, attack_kind)
+
+    def get_damage(self, power, attack_kind):
+        if self.attack_kind == attack_kind:
+            self.hp += power
+        else:
+            self.hp -= power
+
+    def get_attack_kind(self):
+        return self.attack_kind
+
+
+class IceMonster(Monster):
+    def __init__(self, name='Ice Monster', hp=50, power=10):
+        super().__init__(name, hp, power)
+        self.attack_kind = 'ICE'
+
+
+class FireMonster(Monster):
+    def __init__(self, name='Fire Monster', hp=50, power=20):
+        super().__init__(name, hp, power)
+        self.attack_kind = 'FIRE'
+
+    def fireball(self):
+        print('FIREBALL!')
+
+
+# 실행 부분 만들기
+if __name__ == "__main__":
+    player = Player('sword master', 100, 30, 'ICE')
+    monsters = []
+    monsters.append(IceMonster())
+    monsters.append(FireMonster())
+
+    for monster in monsters:
+        print(monster)
+
+    for monster in monsters:
+        player.attack(monster, 'ICE')
+
+    print('after the player attacked')
+
+    for monster in monsters:
+        print(monster)
+    print('')
+
+    print(player)
+
+    for monster in monsters:
+        monster.attack(player, monster.get_attack_kind())
+    print('after monsters attacked')
+
+    print(player)
